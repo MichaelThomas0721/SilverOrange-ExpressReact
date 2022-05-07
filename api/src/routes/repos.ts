9be2f6e7d6c5f4a4dc7a34961538e3b9695d07fe
Michael <michaelthomas0721@icloud.com>
2022-05-07 13:59:00 -0400

    feat: api: implemented GitHub API request for fetching repo information

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,12 +1,35 @@
-import { Router, Request, Response } from 'express';
+import { Request, Response, Router } from 'express';
+
+import request from 'request';
 
 export const repos = Router();
 
 repos.get('/', async (_: Request, res: Response) => {
+  //callback function that will be used to call the github api and get the repos
+  function callback(
+    error: any,
+    response: { statusCode: number },
+    body: string
+  ) {
+    if (!error && response.statusCode === 200) {
+      const data = JSON.parse(body);
+      res.json(data);
+    }
+  }
+
+  const options = {
+    url: 'https://api.github.com/users/silverorange/repos',
+    headers: {
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      'User-Agent': 'request',
+    },
+  };
+
   res.header('Cache-Control', 'no-store');
 
   res.status(200);
 
+  request(options, callback);
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+  //res.json([]);
 });
