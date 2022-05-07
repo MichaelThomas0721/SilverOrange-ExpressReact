69f22eff5686d0ac4b793a6bedc27c8f9e56acd1
Michael <michaelthomas0721@icloud.com>
2022-05-07 15:16:26 -0400

    feat: web: mapped repo data to repo elements

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,17 @@
 import './App.css';
 
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
+import RepoBox from './components/RepoBox';
 import RepoType from './types/RepoType';
 import axios from 'axios';
+
+const apiUrl = 'http://localhost:4000/repos';
 
 export function App() {
   //Used to hold a main copy of the data from the API
   const repoMain = useRef([] as RepoType[]);
+  const [repos, setRepos] = useState([] as RepoType[]);
 
   useEffect(() => {
     getData();
@@ -16,19 +20,31 @@ export function App() {
 
   async function getData() {
     try {
-      await axios.get('http://localhost:4000/repos').then((res) => {
+      await axios.get(apiUrl).then((res) => {
         repoMain.current = res.data as RepoType[];
       });
     } catch (error) {
       // eslint-disable-next-line no-console
       console.log(error);
     }
-    // eslint-disable-next-line no-console
-    console.log(repoMain.current);
+
+    setRepos(repoMain.current);
   }
   return (
     <div className="App">
       <header className="App-header">
+        {repos.map((repo: RepoType, index: number) => (
+          <div key={index}>
+            <RepoBox
+              id={repo.id}
+              name={repo.name}
+              description={repo.description}
+              language={repo.language}
+              forks={repo.forks_count}
+              created_at={repo.created_at}
+            />
+          </div>
+        ))}
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
