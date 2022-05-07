22f3ed2d1ba3a87ff73d60d8dc9527448e9ae541
Michael <michaelthomas0721@icloud.com>
2022-05-07 14:37:00 -0400

    feat: web: fetched data from express API and logged data to console

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-
 import './App.css';
 
+import React, { useEffect } from 'react';
+
+import axios from 'axios';
+
+const apiUrl = 'http://localhost:4000/repos';
+
 export function App() {
+  useEffect(() => {
+    getData();
+  }, []);
+
+  async function getData() {
+    try {
+      await axios.get(apiUrl).then((res) => {
+        console.log(res.data);
+      });
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.log(error);
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
